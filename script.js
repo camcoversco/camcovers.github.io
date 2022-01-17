4b4bf9cde6e78caa630001e063761cd82cbb5365
camcoversco <camcoversco@outlook.com>
2022-01-17 15:27:31 +0000

    ul

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ let scale = 1;
  
 switch(myParam) {
   case "mx5":
-    $(".amazon-links").append(`<li><a href="https://geni.us/DressUpBolts">Dress Up Kit (Amazon)</a></li>`)
-    $(".amazon-links").append(`<li><a href="https://geni.us/qEpj">Oil Cap (Amazon)</a></li>`)
-    $(".amazon-links").append(`<li><a href="https://geni.us/BXgAo">Hoses (Amazon)</a></li>`)
+    $(".amazon-links > ul").append(`<li><a href="https://geni.us/DressUpBolts">Dress Up Kit (Amazon)</a></li>`)
+    $(".amazon-links  > ul").append(`<li><a href="https://geni.us/qEpj">Oil Cap (Amazon)</a></li>`)
+    $(".amazon-links > ul").append(`<li><a href="https://geni.us/BXgAo">Hoses (Amazon)</a></li>`)
 
     MODEL_PATH = "https://camcoversco.github.io/mx5.glb";
     INITIAL_MAP = [
@@ -64,9 +64,9 @@ switch(myParam) {
 
     break;
   default: 
-  $(".amazon-links").append(`<li><a href="https://geni.us/DressUpBolts">Dress Up Kit (Amazon)</a></li>`)
-  $(".amazon-links").append(`<li><a href="https://geni.us/qEpj">Oil Cap (Amazon)</a></li>`)
-  $(".amazon-links").append(`<li><a href="https://geni.us/BXgAo">Hoses (Amazon)</a></li>`)
+  $(".amazon-links > ul").append(`<li><a href="https://geni.us/DressUpBolts">Dress Up Kit (Amazon)</a></li>`)
+  $(".amazon-links > ul").append(`<li><a href="https://geni.us/qEpj">Oil Cap (Amazon)</a></li>`)
+  $(".amazon-links > ul").append(`<li><a href="https://geni.us/BXgAo">Hoses (Amazon)</a></li>`)
 
     MODEL_PATH = "https://camcoversco.github.io/mx5.glb";
     INITIAL_MAP = [
